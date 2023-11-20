f37ac4ebe27b4355555b21b4165dd53ef955fa17
adharshenp <adharshenp07@gmail.com>
2023-11-20 11:39:17 +0530

    final version of counter app

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import Counter from './components/Counter';
 
 function App() {
   return (
-    <div className="d-flex align-t=items-center justify-content-center w-100 flex-column"style={{height:'100vh'}}>
-    <div className='d-flex align-items-center justify-content-center flex-column p-5 rounded' style={{backgroundColor:'yellow', color:"red"}}>
+    <div className="d-flex align-t=items-center justify-content-center w-100 flex-column"style={{height:'100vh',backgroundColor:'black'}}>
+    <div className='d-flex align-items-center justify-content-center flex-column p-5 rounded' style={{backgroundColor:'white', color:"red",margin:'35rem'}}>
       <h1 className='text-primary'>Counter Application</h1>
       <Counter></Counter>
     </div>
